def4aff96b2544d55313006ac7c1e191726fe6ee
Skyler Home Desktop <shunsinger643@gmail.com>
2023-03-06 23:27:46 -0800

    creating repo from intellij

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        try {
+            String to = "shunsinger643@gmail.com";
+            String from = "noreply.diskotek@gmail.com";
+            String subject = "Test 001";
+            String body = "This is a Java Mail test";
+            SendMail client = new SendMail(
+                    host, port, user, pass,
+                    EncType, auth);
+
+            )
+        }
+    }
+}
